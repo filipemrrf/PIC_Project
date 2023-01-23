69ade7e75cc1473650135c9806c82cd68f976a4c
filipemrrf <filipe.ficalho@gmail.com>
2023-01-23 13:15:47 +0000

    Correction to the file format

--- Convergence_Tests/IC_Writer-Gauss.py
+++ Convergence_Tests/IC_Writer-Gauss.py
@@ -18,18 +18,14 @@ for i in range(0, 5):
     # Opens the file that will be written
     FILE = open(("Data/IC-Gauss-" + str(NPoints) + "p.dat"), "w")
 
-    """# Writtes the number of equations in the ODE systemspace step, the number of points to the file and the parameters for the equation
+    # Writtes the number of equations in the ODE systemspace step, the number of points to the file and the parameters for the equation
     FILE.write("#NEq: 2\n")
     FILE.write("#step_x: " + str(1/NPoints) + "\n")
     FILE.write("#NPoints: " + str(NPoints) + "\n")
-    FILE.write("#pars: 1 1.0\n")"""
-
-    FILE.write("\"Time = 0.0\n")
+    FILE.write("#pars: 1 1.0\n")
 
     # Writtes the initial conditions for the equation
     for j in range(NPoints - 1):
-        FILE.write(str(j/NPoints) + " ")
-
         FILE.write(str(exp(-(j/NPoints)*(j/NPoints)/0.1)))
         FILE.write(" ")
         FILE.write(str(-0.1*(j/NPoints)*exp(-(j/NPoints)*(j/NPoints)*0.005)))
