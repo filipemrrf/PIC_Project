2b4e418f56c3e6ee8d66897c6547bad1087284da
filipemrrf <filipe.ficalho@gmail.com>
2023-05-05 09:58:29 +0100

    IC writer update

--- IC_Writers/IC_Writer-Gauss.py
+++ IC_Writers/IC_Writer-Gauss.py
@@ -2,8 +2,8 @@
  " @file IC_Writer-Gauss.py
  " @author Filipe Ficalho (filipe.ficalho@tecnico.ulisboa.pt)
  " @brief Writes the initial conditions for the main executable to run
- " @version 2.0
- " @date 2023-02-27
+ " @version 3.0
+ " @date 2023-04-18
  " 
  " @copyright Copyright (c) 2023
  " 
@@ -16,33 +16,29 @@ std_dev = 0.1
 c = 1/(std_dev*std_dev)
 
 # Sets the full size of the space and the space of interest
-full_size = 30
-relevant_size = 5
-size_ratio = full_size/relevant_size
+size = 5
 
 # Initializes the number of points
-NPoints = int(50*size_ratio)
+NPoints = int(50*size)
 
 # Loops through all the files that will be written
 for i in range(0, 5):
     # Opens the file that will be written
-    FILE = open(("Data/IC-Gauss-" + str(NPoints) + "p.dat"), "w")
+    FILE_Phi = open(("Initial_Conditions/IC-exp(-50x^2)-" + str(NPoints) + "p.dat"), "w")
+    FILE_Pi = open(("Initial_Conditions/IC-(-100).exp(-50x^2)-" + str(NPoints) + "p.dat"), "w")
 
     # Writtes the initial conditions for the equation
-    for j in range(NPoints-1):
+    for j in range(NPoints):
         # Devines an auxiliary variable x
-        x = j*relevant_size/NPoints
+        x = j*size/NPoints
 
         # Writes the IC to the file
-        FILE.write(str(exp(-0.5*c*x*x)))
-        FILE.write(" ")
-        FILE.write(str(-x*c*exp(-0.5*c*x*x)))
-        FILE.write("\n")
-
-    FILE.write("0.0 0.0")
+        FILE_Phi.write(str(x) + " " + str(exp(-0.5*c*x*x)) + "\n")
+        FILE_Pi.write(str(x) + " " + str(-c*exp(-0.5*c*x*x)) + "\n")
 
     # Closes the file
-    FILE.close()
+    FILE_Phi.close()
+    FILE_Pi.close()
 
     # Updates how many points the next initial conditions will have
     NPoints *= 2
--- IC_Writers/IC_Writer-Sin.py
+++ IC_Writers/IC_Writer-Sin.py
@@ -2,8 +2,8 @@
  " @file IC_Writer-Sin.py
  " @author Filipe Ficalho (filipe.ficalho@tecnico.ulisboa.pt)
  " @brief Writes the initial conditions for the main executable to run
- " @version 2.2
- " @date 2023-02-16
+ " @version 3.0
+ " @date 2023-03-28
  " 
  " @copyright Copyright (c) 2023
  " 
@@ -16,19 +16,17 @@ NPoints = 50
 
 for i in range(0, 5):
     # Opens the file that will be written
-    FILE = open(("Data/IC-Sin-" + str(NPoints) + "p.dat"), "w")
+    FILE_PHI = open(("Initial_Conditions/IC-Sin(2pi.x)-" + str(NPoints) + "p.dat"), "w")
+    FILE_PI = open(("Initial_Conditions/IC-2pi.Cos(2pi.x)-" + str(NPoints) + "p.dat"), "w")
 
     # Writtes the initial conditions for the equation
     for j in range(NPoints):
-        FILE.write(str(numpy.sin(2*numpy.pi*j/NPoints)))
-        FILE.write(" ")
-        FILE.write(str(2*numpy.pi*numpy.cos(2*numpy.pi*j/NPoints)))
-        FILE.write("\n")
-
-    FILE.write("0.0 " + str(2*numpy.pi))
+        FILE_PHI.write(str(j/NPoints) + " " + str(numpy.sin(2*numpy.pi*(j/NPoints))) + "\n")
+        FILE_PI.write(str(j/NPoints) + " " + str(2*numpy.pi*numpy.cos(2*numpy.pi*(j/NPoints))) + "\n")
 
     # Closes the file
-    FILE.close()
+    FILE_PHI.close()
+    FILE_PI.close()
 
     # Updates how many points the next initial conditions will have
     NPoints *= 2
